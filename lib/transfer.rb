2ed010e0efaeac2f44a5f0b4c4ae8a66539610f1
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-24 01:51:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,9 +12,12 @@ class Transfer
   end
 
   def execute_transaction
-    self.sender.balance -= self.amount
-    self.receiver.balance += self.amount
-    self.status = "complete"
+    if self.valid?
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount
+      self.status = "complete"
+    else
+      "Transaction rejected. Please check your account balance."
   end
 
   def reverse_transfer
