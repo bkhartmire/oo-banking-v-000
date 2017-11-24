6a81064395d2355466bdbbe268376449af4ef230
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-24 01:56:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,8 @@ class Transfer
       self.receiver.balance += self.amount
       self.status = "complete"
     else
-      return "Transaction rejected. Please check your account balance."
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 
